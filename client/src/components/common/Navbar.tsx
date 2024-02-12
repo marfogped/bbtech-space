31515319421ddc08bd51a138734603542d902057
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-12 15:06:56 -0300

    feat: Hero & Navbar components

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Logo } from "../../lib/images";
+// import { useTranslation } from 'react-i18next';
 
 const Navbar: React.FC = () => {
-  return (
-    <nav>
-      <div>logo</div>
+  // const { t } = useTranslation();
 
-      <nav></nav>
+  return (
+    <nav className="z-50 w-full h-max fixed">
+      <div className="section-container flex items-center justify-between py-4">
+        <div className="flexCenter">
+          <img src={Logo} alt="bbtech logo" className="h-14 w-auto" />
+        </div>
+        <ul className="flex items-center gap-5 font-zenKaku">
+          <li>What We Do</li>
+          <li>Services</li>
+          <li>Jobs</li>
+          <li>Sponsors</li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- client/src/components/landing-page/hero/Hero.tsx
+++ client/src/components/landing-page/hero/Hero.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { HeroImg } from "../../../lib/images";
 
 const Hero: React.FC = () => {
   return (
-    <header>
-      <div className="font-vt323">Hero</div>
+    <header className="w-full h-screen">
+      <div className="section-container grid grid-cols-5 gap-5">
+        <div className="col-span-3 flex justify-center flex-col gap-5">
+          <div className="flex flex-col gap-5">
+            <h1 className="text-8xl font-vt323">
+              Next Level of{" "}
+              <span className="bg-purplePrimary block w-max text-black">
+                IT RECRUITMENT
+              </span>{" "}
+            </h1>
+            <p className="text-2xl text-balance text-neutral/90">
+              We connect the objectives of companies with the goals and
+              aspirations of candidates, thereby fostering mutual growth and
+              success.
+            </p>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <button className="border-2 border-neutral px-12 py-2 font-vt323 text-2xl">
+              Learn More
+            </button>
+            <button className="border-2 border-purplePrimary bg-purplePrimary text-black px-12 py-2 font-vt323 text-2xl">
+              Get Started
+            </button>
+          </div>
+        </div>
+
+        <div className="col-span-2 flexCenter">
+          <img src={HeroImg} alt="hero image" className="w-full h-auto" />
+        </div>
+      </div>
     </header>
   );
 };
