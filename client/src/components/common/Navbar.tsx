23f76eb65f8cd0f4f34df42fca91fc33944ef05b
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-12 16:43:46 -0300

    feat: mobile styles

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Logo } from "../../lib/images";
+import useWindowDimensions from "../../lib/useWindowDimensions";
+// import { ScrollTo } from "..";
 // import { useTranslation } from 'react-i18next';
 
 const Navbar: React.FC = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [color, setColor] = useState<boolean>(false);
+  const { windowWidth } = useWindowDimensions();
   // const { t } = useTranslation();
 
+  const openNav = () => {
+    setIsActive(!isActive);
+  };
+
+  const changeColor = () => {
+    if (window.scrollY >= 60) {
+      setColor(true);
+    } else if (window.scrollY <= 60) {
+      setColor(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+    };
+  }, []);
   return (
-    <nav className="z-50 w-full h-max fixed">
+    <nav
+      className={`z-50 w-screen h-max fixed transition-all duration-300 ${
+        color ? "backdrop-blur-md" : ""
+      }`}
+    >
       <div className="section-container flex items-center justify-between py-4">
         <div className="flexCenter">
           <img src={Logo} alt="bbtech logo" className="h-14 w-auto" />
         </div>
-        <ul className="flex items-center gap-5 font-zenKaku">
-          <li>What We Do</li>
-          <li>Services</li>
-          <li>Jobs</li>
-          <li>Sponsors</li>
-        </ul>
+
+        {windowWidth > 768 ? (
+          <ul className="flex items-center gap-5 font-zenKaku text-xl">
+            <li>What We Do</li>
+            <li>Services</li>
+            <li>Jobs</li>
+            <li>Sponsors</li>
+          </ul>
+        ) : (
+          <div className="menu-toggle xs:flex sm:flex md:hidden">
+            <div
+              onClick={openNav}
+              id="menu-toggle-btn"
+              className={isActive ? "active" : ""}
+            >
+              <span className={color ? "black" : "white"}></span>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
--- client/src/components/landing-page/hero/Hero.tsx
+++ client/src/components/landing-page/hero/Hero.tsx
@@ -4,33 +4,33 @@ import { HeroImg } from "../../../lib/images";
 const Hero: React.FC = () => {
   return (
     <header className="w-full h-screen">
-      <div className="section-container grid grid-cols-5 gap-5">
-        <div className="col-span-3 flex justify-center flex-col gap-5">
+      <div className="section-container grid xs:grid-cols-1 sm:grid-cols-1 lg:grid-cols-5 gap-5">
+        <div className="lg:col-span-3 flex xs:justify-end sm:justify-end md:justify-center flex-col gap-5">
           <div className="flex flex-col gap-5">
-            <h1 className="text-8xl font-vt323">
+            <h1 className="xs:text-6xl sm:text-7xl lg:text-8xl font-vt323">
               Next Level of{" "}
               <span className="bg-purplePrimary block w-max text-black">
                 IT RECRUITMENT
               </span>{" "}
             </h1>
-            <p className="text-2xl text-balance text-neutral/90">
+            <p className="xs:text-xl sm:text-xl md:text-2xl font-zenKaku text-balance text-neutral/90">
               We connect the objectives of companies with the goals and
               aspirations of candidates, thereby fostering mutual growth and
               success.
             </p>
           </div>
 
-          <div className="flex items-center gap-5">
-            <button className="border-2 border-neutral px-12 py-2 font-vt323 text-2xl">
+          <div className="flex xs:flex-col sm:flex-col md:flex-row items-center gap-5">
+            <button className="border-2 xs:w-full sm:w-full md:w-auto border-neutral px-12 py-2 font-vt323 text-2xl">
               Learn More
             </button>
-            <button className="border-2 border-purplePrimary bg-purplePrimary text-black px-12 py-2 font-vt323 text-2xl">
+            <button className="border-2 xs:w-full sm:w-full md:w-auto border-purplePrimary bg-purplePrimary text-black px-12 py-2 font-vt323 text-2xl">
               Get Started
             </button>
           </div>
         </div>
 
-        <div className="col-span-2 flexCenter">
+        <div className="lg:col-span-2 flexCenter">
           <img src={HeroImg} alt="hero image" className="w-full h-auto" />
         </div>
       </div>
