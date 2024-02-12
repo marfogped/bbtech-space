4eec5e518da9a326107f2c4a9ade424ea060e785
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-12 13:15:10 -0300

    add: Images for the entire website

---
+++ client/src/lib/constants.ts
@@ -0,0 +1,7 @@
+import HeroImg from "../assets/img/hero.png";
+import AboutImg from "../assets/img/about.png";
+import AntennaImg from "../assets/img/antenna.png";
+import Satelite1 from "../assets/img/satelite-1.png";
+import Satelite2 from "../assets/img/satelite-2.png";
+
+export { HeroImg, AboutImg, AntennaImg, Satelite1, Satelite2 };
