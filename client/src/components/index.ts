4ddf7d0e162694b3c49e48f3c15e4ff2918ce2f1
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-12 16:44:32 -0300

    add: useWindowDimensions hook & ScrollTo component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Navbar } from "./common/Navbar";
 export { default as MatrixRain } from "./common/MatrixRain";
+export { default as ScrollTo } from "./common/ScrollTo";
 
 export { default as Hero } from "./landing-page/hero/Hero";
 export { default as About } from "./landing-page/about/About";
