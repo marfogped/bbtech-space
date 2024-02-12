096cc1a79aa89762a7e6dc87c837f743e4e7c184
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-02-12 15:05:45 -0300

    fix: image.ts file created to images import & logo added

---
+++ client/src/lib/images.ts
@@ -0,0 +1,8 @@
+import HeroImg from "../assets/img/hero.png";
+import AboutImg from "../assets/img/about.png";
+import AntennaImg from "../assets/img/antenna.png";
+import Satelite1 from "../assets/img/satelite-1.png";
+import Satelite2 from "../assets/img/satelite-2.png";
+import Logo from "../assets/img/logo.svg";
+
+export { HeroImg, AboutImg, AntennaImg, Satelite1, Satelite2, Logo };
