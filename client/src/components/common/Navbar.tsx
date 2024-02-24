147c2e6c448c1ba1ac5454e4123225bf1b1d76a2
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-02-23 22:36:34 -0300

    feat: mobile navbar

--- client/src/components/common/Navbar.tsx
+++ client/src/components/common/Navbar.tsx
@@ -28,19 +28,57 @@ const Navbar: React.FC = () => {
     };
   }, []);
   return (
-    <nav
-      className={`z-50 w-screen h-max fixed transition-all duration-300 ${
-        color ? "backdrop-blur-md" : ""
-      }`}
-    >
-      <div className="section-container flex items-center justify-between py-4">
-        <div className="flexCenter">
-          <img src={Logo} alt="bbtech logo" className="h-14 w-auto" />
-        </div>
+    <>
+      <nav
+        className={`z-50 w-screen h-max fixed transition-all duration-300 ${
+          color && !isActive ? "backdrop-blur-md" : ""
+        }`}
+      >
+        <div className="section-container flex items-center justify-between py-4">
+          <div className="flexCenter">
+            <img src={Logo} alt="bbtech logo" className="h-14 w-auto" />
+          </div>
 
-        {windowWidth > 768 ? (
-          <div className="flex items-center gap-4">
-            <ul className="flex items-center gap-5 font-zenKaku text-xl">
+          {windowWidth > 768 ? (
+            <div className="flex items-center gap-4">
+              <ul className="flex items-center gap-5 font-zenKaku text-xl">
+                <li>
+                  <ScrollTo id="about">
+                    {t('about')}
+                  </ScrollTo>
+                </li>
+                <li>
+                  <ScrollTo id="services">
+                    {t('services')}
+                  </ScrollTo>
+                </li>
+                <li>
+                  <ScrollTo id="jobs">
+                    {t('jobs')}
+                  </ScrollTo>
+                </li>
+              </ul>
+
+              <LanguageSelector />
+            </div>
+          ) : (
+            <div className="menu-toggle xs:flex sm:flex md:hidden">
+              <div
+                onClick={openNav}
+                id="menu-toggle-btn"
+                className={isActive ? "active" : ""}
+              >
+                <span className={"white"}></span>
+              </div>
+            </div>
+          )}
+        </div>
+      </nav>
+
+      {
+        windowWidth < 768 ? (
+          <nav className={`${isActive ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4 pointer-events-none"} backdrop-blur-md z-40 transition-all fixed top-0 left-0 w-full h-max p-4 flex flex-col items-center`}>
+            <ul className="flex flex-col h-max items-center gap-5 font-zenKaku text-3xl mb-8 pt-20">
               <li>
                 <ScrollTo id="about">
                   {t('about')}
@@ -59,20 +97,10 @@ const Navbar: React.FC = () => {
             </ul>
 
             <LanguageSelector />
-          </div>
-        ) : (
-          <div className="menu-toggle xs:flex sm:flex md:hidden">
-            <div
-              onClick={openNav}
-              id="menu-toggle-btn"
-              className={isActive ? "active" : ""}
-            >
-              <span className={"white"}></span>
-            </div>
-          </div>
-        )}
-      </div>
-    </nav>
+          </nav>
+        ) : ("")
+      }   
+    </>
   );
 };
 
