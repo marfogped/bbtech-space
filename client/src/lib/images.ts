cd815b5d82b366ada80a8b75ede8669001181121
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-02-27 18:27:03 -0300

    fix: images with .png replaced for the same ones with .webp extgension

--- client/src/lib/images.ts
+++ client/src/lib/images.ts
@@ -1,8 +1,8 @@
 import HeroImg from "../assets/img/hero.png";
 import AboutImg from "../assets/img/about.png";
-import AntennaImg from "../assets/img/antenna.png";
-import Satelite1 from "../assets/img/satelite-1.png";
-import Satelite2 from "../assets/img/satelite-2.png";
+import AntennaImg from "../assets/img/antenna.webp";
+import Satelite1 from "../assets/img/satelite-1.webp";
+import Satelite2 from "../assets/img/satelite-2.webp";
 import Logo from "../assets/img/logo.svg";
 
 export { HeroImg, AboutImg, AntennaImg, Satelite1, Satelite2, Logo };
