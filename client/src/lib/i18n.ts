a54dc0c80387f8660eba6f1807d70510b74cfad5
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-02-29 18:47:55 -0300

    fix: debug option deleted

--- client/src/lib/i18n.ts
+++ client/src/lib/i18n.ts
@@ -3,20 +3,26 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-i18n
-  .use(HttpBackend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: "en",
-    debug: true,
-    ns: ['navbar', 'hero', 'about', 'services', 'jobs', 'testimonials', 'footer'],
-    interpolation: {
-      escapeValue: false,
-    },
-    backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
-    },
-  });
+i18n.use(HttpBackend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: "en",
+        ns: [
+            "navbar",
+            "hero",
+            "about",
+            "services",
+            "jobs",
+            "testimonials",
+            "footer",
+        ],
+        interpolation: {
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: "/locales/{{lng}}/{{ns}}.json",
+        },
+    });
 
 export default i18n;
