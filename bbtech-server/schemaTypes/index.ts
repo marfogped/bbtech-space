e0b8afbb8590f02b0810b6ecc9e93a23fff14257
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-02-28 23:29:08 -0300

    add: Sanity.io server base

---
+++ bbtech-server/schemaTypes/index.ts
@@ -0,0 +1,13 @@
+import home from "./home"
+import localizedString from "./localizedString"
+import services from "./services"
+import jobs from "./jobs"
+import testimonials from "./testimonials"
+
+export const schemaTypes = [
+    home,
+    services,
+    jobs,
+    testimonials,
+    localizedString,
+]
