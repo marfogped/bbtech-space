787fded0f7a41356bfe3615e2ee6ac72939a8079
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-03-01 17:40:15 -0300

    fix: comma deleted at the end of main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,15 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SanityProvider } from './contexts/SanityContext.tsx';
-import './index.css'
+import { SanityProvider } from "./contexts/SanityContext.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <SanityProvider>
     <Router>
       <Routes>
-        <Route path='/*' element={<App />} />
+        <Route path="/*" element={<App />} />
       </Routes>
-    </Router>,
+    </Router>
   </SanityProvider>
-)
+);
