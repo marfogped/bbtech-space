545ac9715eeef20c9c9de825124eed403e73b30f
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-03-01 17:39:38 -0300

    fix: GROQ Validations added to get appropiate language & translate function includes arrays in tranlation object

--- client/src/contexts/SanityContext.tsx
+++ client/src/contexts/SanityContext.tsx
@@ -5,57 +5,81 @@ import { useTranslation } from "react-i18next";
 import { transformSanityArrayToTranslationsObject } from "../lib/utils";
 
 interface SanityContextProps {
-    getHomePage: (language: string) => Promise<HomeProps[]>;
-    setLanguage: (language: string) => void;
-    isLoading: boolean;
-    fetchError: boolean;
-    language: string;
+  getHomePage: (language: string) => Promise<HomeProps[]>;
+  setLanguage: (language: string) => void;
+  isLoading: boolean;
+  fetchError: boolean;
+  language: string;
 }
 
 interface SanityProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
-export const SanityContext = React.createContext<SanityContextProps | null>(null);
+export const SanityContext = React.createContext<SanityContextProps | null>(
+  null
+);
 
 export const SanityProvider = ({ children }: SanityProviderProps) => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [fetchError, setFetchError] = useState<boolean>(false);
-    const { i18n } = useTranslation();
-    const [language, setLanguage] = useState(i18n.language);
-  
-    const getHomePage = async ( language: string ) => {
-      try {
-        const query = `*[_type == "home"]{
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [fetchError, setFetchError] = useState<boolean>(false);
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+
+  const getHomePage = async (language: string) => {
+    try {
+      const query = `*[_type == "home"]{
           ...,
+          "services": services[]-> {
+            ...,
+            "description": description.${language},
+            "title": title.${language},
+            "tag": tag.${language},
+            "btn": btn.${language}
+          },
+          "testimonials": testimonials[]-> {
+            ...,
+            "description": description.${language}
+          },
+          "jobs": jobs[]-> {
+            ...,
+            "areas": areas[]{
+              "${language}": ${language}
+            },
+            "companyIcon" : companyIcon.asset->url
+          },
         }`;
-        
-        const homeResult = await client.fetch(query);
 
-        if (homeResult) {
-            const translations = transformSanityArrayToTranslationsObject(homeResult, language);
-            i18n.changeLanguage(language);
-            setIsLoading(false);
-            i18n.addResourceBundle(language, 'home', translations);
-            return homeResult;
-        }
-        return;
-      } catch (error) {
+      const homeResult = await client.fetch(query);
+
+      if (homeResult) {
+        const translations = transformSanityArrayToTranslationsObject(
+          homeResult,
+          language
+        );
+        i18n.changeLanguage(language);
         setIsLoading(false);
-        setFetchError(true);
-        console.log(error);
+        console.log(translations);
+        i18n.addResourceBundle(language, "home", translations);
+        return homeResult;
       }
-    };
-
-    const value: SanityContextProps = {
-      getHomePage,
-      setLanguage,
-      isLoading,
-      fetchError,
-      language,
-    };
-  
-    return (
-      <SanityContext.Provider value={value}>{children}</SanityContext.Provider>
-    );
+      return;
+    } catch (error) {
+      setIsLoading(false);
+      setFetchError(true);
+      console.log(error);
+    }
   };
+
+  const value: SanityContextProps = {
+    getHomePage,
+    setLanguage,
+    isLoading,
+    fetchError,
+    language,
+  };
+
+  return (
+    <SanityContext.Provider value={value}>{children}</SanityContext.Provider>
+  );
+};
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,35 +1,43 @@
 interface LocalizedString {
-    en?: string;
-    es?: string;
-    it?: string;
-    _type: "localizedString";
+  en?: string;
+  es?: string;
+  it?: string;
+  _type: "localizedString";
 }
 
 interface TranslationItem {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [key: string]: LocalizedString | any;
+  [key: string]: LocalizedString | LocalizedString[];
 }
 
 export const transformSanityArrayToTranslationsObject = (
-    sanityArray: TranslationItem[],
-    language: string
-): { [key: string]: string } => {
-    const translationsObject: { [key: string]: string } = {};
+  sanityArray: TranslationItem[],
+  language: string
+): { [key: string]: string | LocalizedString[] } => {
+  const translationsObject: { [key: string]: string | LocalizedString[] } = {};
 
-    sanityArray.forEach((item) => {
-        Object.keys(item).forEach((key) => {
-            const value = item[key];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const isValidLanguageKey = (key: any): key is keyof LocalizedString => {
+    return ["en", "es", "it"].includes(key);
+  };
 
-            if (
-                value &&
-                typeof value === "object" &&
-                value._type === "localizedString" &&
-                value[language]
-            ) {
-                translationsObject[key] = value[language];
-            }
-        });
+  sanityArray.forEach((item) => {
+    Object.keys(item).forEach((key) => {
+      const value = item[key];
+      if (Array.isArray(value)) {
+        // Asigna directamente el array sin modificar su contenido.
+        translationsObject[key] = value;
+      } else if (
+        value &&
+        typeof value === "object" &&
+        value._type === "localizedString" &&
+        isValidLanguageKey(language) &&
+        value[language]
+      ) {
+        // Ahora es seguro acceder a 'value[language]' gracias a la comprobación de tipo.
+        translationsObject[key] = value[language] as string;
+      }
     });
+  });
 
-    return translationsObject;
+  return translationsObject;
 };
