7ea58a79c99e098fa647ea70c6dc6ca9f62831f9
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-03-02 00:45:46 -0300

    fix: Hero image and About image replaced for the same images, in different formats, and low size

--- client/src/lib/images.ts
+++ client/src/lib/images.ts
@@ -1,8 +1,20 @@
-import HeroImg from "../assets/img/hero.png";
-import AboutImg from "../assets/img/about.png";
+import HeroImg from "../assets/img/hero.webp";
+import AboutImg from "../assets/img/about.webp";
 import AntennaImg from "../assets/img/antenna.webp";
 import Satelite1 from "../assets/img/satelite-1.webp";
 import Satelite2 from "../assets/img/satelite-2.webp";
 import Logo from "../assets/img/logo.svg";
 
-export { HeroImg, AboutImg, AntennaImg, Satelite1, Satelite2, Logo };
+import WorkersServiceImage from "../assets/img/worker-service.webp";
+import CompaniesServiceImage from "../assets/img/company-service.webp";
+
+export {
+  HeroImg,
+  AboutImg,
+  AntennaImg,
+  Satelite1,
+  Satelite2,
+  Logo,
+  WorkersServiceImage,
+  CompaniesServiceImage,
+};
