046e515a8479a8ce8b19648bfd6e25b868a11a0c
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-20 14:17:42 -0300

    fix(translations): Translations Bug on mount with new devices

--- client/src/lib/i18n.ts
+++ client/src/lib/i18n.ts
@@ -3,11 +3,47 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+const customLanguageDetector = {
+    name: "customLanguageDetector",
+    lookup() {
+        const storedLang = localStorage.getItem("i18nextLng");
+        if (storedLang) {
+            return storedLang.split("-")[0];
+        }
+
+        return null;
+    },
+    cacheUserLanguage(lng: string) {
+        localStorage.setItem("i18nextLng", lng.split("-")[0]);
+    },
+};
+
 i18n.use(HttpBackend)
     .use(LanguageDetector)
+
+    .use({
+        type: "languageDetector",
+        async: false,
+        init: () => {},
+        detect: customLanguageDetector.lookup,
+        cacheUserLanguage: customLanguageDetector.cacheUserLanguage,
+    })
     .use(initReactI18next)
     .init({
         fallbackLng: "en",
+        load: "languageOnly",
+        detection: {
+            order: [
+                "customLanguageDetector",
+                "navigator",
+                "querystring",
+                "cookie",
+                "localStorage",
+                "sessionStorage",
+                "htmlTag",
+            ],
+            caches: ["localStorage"],
+        },
         ns: [
             "navbar",
             "hero",
@@ -20,13 +56,6 @@ i18n.use(HttpBackend)
             "privacy",
             "contact",
         ],
-        interpolation: {
-            escapeValue: false,
-        },
-        react: {
-            bindI18n: "languageChanged",
-            useSuspense: true,
-        },
     });
 
 export default i18n;
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,43 +1,42 @@
 export interface LocalizedString {
-  en?: string;
-  es?: string;
-  it?: string;
-  _type: "localizedString";
+    en?: string;
+    es?: string;
+    it?: string;
+    _type: "localizedString";
 }
 
 interface TranslationItem {
-  [key: string]: LocalizedString | LocalizedString[];
+    [key: string]: LocalizedString | LocalizedString[];
 }
 
 export const transformSanityArrayToTranslationsObject = (
-  sanityArray: TranslationItem[],
-  language: string
+    sanityArray: TranslationItem[],
+    language: string
 ): { [key: string]: string | LocalizedString[] } => {
-  const translationsObject: { [key: string]: string | LocalizedString[] } = {};
+    const translationsObject: { [key: string]: string | LocalizedString[] } =
+        {};
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const isValidLanguageKey = (key: any): key is keyof LocalizedString => {
-    return ["en", "es", "it"].includes(key);
-  };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const isValidLanguageKey = (key: any): key is keyof LocalizedString => {
+        return ["en", "es", "it"].includes(key);
+    };
 
-  sanityArray.forEach((item) => {
-    Object.keys(item).forEach((key) => {
-      const value = item[key];
-      if (Array.isArray(value)) {
-        // Asigna directamente el array sin modificar su contenido.
-        translationsObject[key] = value;
-      } else if (
-        value &&
-        typeof value === "object" &&
-        value._type === "localizedString" &&
-        isValidLanguageKey(language) &&
-        value[language]
-      ) {
-        // Ahora es seguro acceder a 'value[language]' gracias a la comprobación de tipo.
-        translationsObject[key] = value[language] as string;
-      }
+    sanityArray.forEach((item) => {
+        Object.keys(item).forEach((key) => {
+            const value = item[key];
+            if (Array.isArray(value)) {
+                translationsObject[key] = value;
+            } else if (
+                value &&
+                typeof value === "object" &&
+                value._type === "localizedString" &&
+                isValidLanguageKey(language) &&
+                value[language]
+            ) {
+                translationsObject[key] = value[language] as string;
+            }
+        });
     });
-  });
 
-  return translationsObject;
+    return translationsObject;
 };
